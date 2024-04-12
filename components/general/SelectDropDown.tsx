7eb0d2480683aec3fddfdeda5ee67c89bc3b864e
Tang Weng Kitt <wengkitt969696@gmail.com>
2024-04-12 17:49:03 +0800

    #5 Add SelectDropDown

--- components/general/SelectDropDown.tsx
+++ components/general/SelectDropDown.tsx
@@ -1,19 +1,31 @@
 import { Check, ChevronDown, ChevronUp } from "@tamagui/lucide-icons";
 import { useMemo, useState } from "react";
-import type { FontSizeTokens, SelectProps } from "tamagui";
-import { Adapt, Label, Select, Sheet, XStack, YStack, getFontSize } from "tamagui";
+import type { SelectProps } from "tamagui";
+import { Adapt, Select, Sheet, YStack, getFontSize } from "tamagui";
 import { LinearGradient } from "tamagui/linear-gradient";
 
-export function SelectDropDown(props: SelectProps) {
-  const [val, setVal] = useState("apple");
+export type DataListModel = {
+  id: string | number;
+  label: string;
+  value: string;
+};
+
+type Props = {
+  selectProps: SelectProps;
+  dataList: DataListModel[];
+};
+
+export function SelectDropDown({ selectProps, dataList }: Props) {
+  const [val, setVal] = useState<string>(dataList[0].value);
+
   return (
-    <Select value={val} onValueChange={setVal} disablePreventBodyScroll {...props}>
-      <Select.Trigger flex={1} width={220} iconAfter={ChevronDown}>
-        <Select.Value placeholder="Something" />
+    <Select value={val} onValueChange={setVal} disablePreventBodyScroll {...selectProps}>
+      <Select.Trigger flex={1} iconAfter={ChevronDown}>
+        <Select.Value placeholder="Select..." />
       </Select.Trigger>
+
       <Adapt when="sm" platform="touch">
         <Sheet
-          native={!!props.native}
           modal
           dismissOnSnapToBottom
           animationConfig={{
@@ -27,10 +39,10 @@ export function SelectDropDown(props: SelectProps) {
               <Adapt.Contents />
             </Sheet.ScrollView>
           </Sheet.Frame>
-
           <Sheet.Overlay animation="lazy" enterStyle={{ opacity: 0 }} exitStyle={{ opacity: 0 }} />
         </Sheet>
       </Adapt>
+
       <Select.Content zIndex={200000}>
         <Select.ScrollUpButton
           alignItems="center"
@@ -41,7 +53,6 @@ export function SelectDropDown(props: SelectProps) {
           <YStack zIndex={10}>
             <ChevronUp size={20} />
           </YStack>
-
           <LinearGradient
             start={[0, 0]}
             end={[0, 1]}
@@ -50,40 +61,33 @@ export function SelectDropDown(props: SelectProps) {
             borderRadius="$4"
           />
         </Select.ScrollUpButton>
-        <Select.Viewport minWidth={200}>
+
+        <Select.Viewport
+          // to do animations:
+          // animation="quick"
+          // animateOnly={['transform', 'opacity']}
+          // enterStyle={{ o: 0, y: -10 }}
+          // exitStyle={{ o: 0, y: 10 }}
+          minWidth={200}>
           <Select.Group>
-            <Select.Label>Fruits</Select.Label>
+            {/* for longer lists memoizing these is useful */}
             {useMemo(
               () =>
-                items.map((item, i) => {
+                dataList.map((data, i) => {
                   return (
-                    <Select.Item index={i} key={item.name} value={item.name.toLowerCase()}>
-                      <Select.ItemText>{item.name}</Select.ItemText>
-
+                    <Select.Item index={i} key={data.id} value={data.value}>
+                      <Select.ItemText>{data.label}</Select.ItemText>
                       <Select.ItemIndicator marginLeft="auto">
                         <Check size={16} />
                       </Select.ItemIndicator>
                     </Select.Item>
                   );
                 }),
-
-              [items]
+              [dataList]
             )}
           </Select.Group>
-          {props.native && (
-            <YStack
-              position="absolute"
-              right={0}
-              top={0}
-              bottom={0}
-              alignItems="center"
-              justifyContent="center"
-              width={"$4"}
-              pointerEvents="none">
-              <ChevronDown size={getFontSize((props.size as FontSizeTokens) ?? "$true")} />
-            </YStack>
-          )}
         </Select.Viewport>
+
         <Select.ScrollDownButton
           alignItems="center"
           justifyContent="center"
@@ -93,7 +97,6 @@ export function SelectDropDown(props: SelectProps) {
           <YStack zIndex={10}>
             <ChevronDown size={20} />
           </YStack>
-
           <LinearGradient
             start={[0, 0]}
             end={[0, 1]}
@@ -106,11 +109,3 @@ export function SelectDropDown(props: SelectProps) {
     </Select>
   );
 }
-const items = [
-  { name: "Apple" },
-  { name: "Pear" },
-  { name: "Blackberry" },
-  { name: "Peach" },
-  { name: "Apricot" },
-  { name: "Melon" },
-];
